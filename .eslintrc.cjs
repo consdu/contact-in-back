ef018498530460eb9dce274d3dc11607a9fd0e14
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-25 09:52:02 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-unused-vars": "error",
+        "no-console": "error",
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
