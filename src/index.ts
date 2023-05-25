660806a1d1f3c97956bcc3f9488135ba9e5a0380
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-25 11:49:23 +0200

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import "./utils//loadEnvironments/loadEnvironments.js";
+import chalk from "chalk";
+import createDebug from "debug";
+import app from "./server/index.js";
 
+const debug = createDebug("contacts-api:root");
+
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug(chalk.green(`Listening on http://localhost:${port}`));
+});
