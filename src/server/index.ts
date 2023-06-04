4011cb899125f6e592d085c88c700d36e6b70eb7
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-04 21:02:33 +0200

    Remove guard that checked if allowed origin env was present

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import morgan from "morgan";
-import createDebug from "debug";
-import chalk from "chalk";
 import cors from "cors";
 import {
   generalError,
@@ -13,14 +11,7 @@ import userRouter from "./routers/user/userRouter.js";
 import { paths } from "../utils/paths.js";
 import contactsRouter from "./routers/contacts/contactsRouter/contactsRouter.js";
 
-const debug = createDebug("contacts-api:root:server");
-
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
-
-if (!allowedOrigin) {
-  debug(chalk.red("Missing allowed origin environment variable"));
-  process.exit(1);
-}
 
 const options: cors.CorsOptions = {
   origin: allowedOrigin,
