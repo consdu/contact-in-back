f23cd6a960a0844df2a6b6d6cb6e2e41e7a9cc9d
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-04 18:39:22 +0200

    Add deleteContact controller

--- src/server/controllers/contacts/contactsControllers.ts
+++ src/server/controllers/contacts/contactsControllers.ts
@@ -1,6 +1,7 @@
 import type { Response, NextFunction } from "express";
 import Contact from "../../../database/models/Contact.js";
 import type { CustomRequest } from "../../types.js";
+import CustomError from "../../../CustomError/CustomError.js";
 
 export const getContacts = async (
   req: CustomRequest,
@@ -12,6 +13,30 @@ export const getContacts = async (
   try {
     const contacts = await Contact.find({ user: userId }).limit(10).exec();
     res.status(200).json({ contacts });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req;
+  const { contactId } = req.params;
+
+  try {
+    const contact = await Contact.findOneAndDelete({
+      _id: contactId,
+      user: userId,
+    }).exec();
+
+    if (contact) {
+      return res.status(204).json({ message: "Contact deleted succesfully" });
+    }
+
+    throw new CustomError(404, "Contact not found");
   } catch (error) {
     next(error);
   }
