ce19f7fbfa2aa1ba69c33cdf57a9f3959e1bb6f2
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-04 19:33:31 +0200

    Add test for deleteContact when contact doesn't exist

--- src/server/controllers/contacts/_tests_/deleteContact.test.ts
+++ src/server/controllers/contacts/_tests_/deleteContact.test.ts
@@ -3,11 +3,16 @@ import Contact from "../../../../database/models/Contact.js";
 import type { CustomRequest } from "../../../types";
 import { databaseContactsMock } from "../../../../mocks/contacts/contactsMock.js";
 import { deleteContact } from "../contactsControllers.js";
+import CustomError from "../../../../CustomError/CustomError.js";
 
 const userId = "test-user-id";
 const contactId = "test-id";
 
 type CustomResponse = Pick<Response, "status" | "json">;
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("Given a deleteContact controller", () => {
   const req: Partial<CustomRequest> = {
@@ -25,16 +30,15 @@ describe("Given a deleteContact controller", () => {
   const next = jest.fn();
 
   describe("When it receives a request and the contact exists", () => {
-    Contact.findOneAndDelete = jest.fn().mockReturnValue({
-      exec: jest.fn().mockResolvedValue(databaseContactsMock[0]),
-    });
-
     test("Then it should call response's method status with 204 and json with 'Contact deleted succesfully'", async () => {
       const expectedStatusCode = 204;
       const expectedJsonResponse = {
         message: "Contact deleted succesfully",
       };
 
+      Contact.findOneAndDelete = jest.fn().mockReturnValue({
+        exec: jest.fn().mockResolvedValue(databaseContactsMock[0]),
+      });
       await deleteContact(
         req as CustomRequest,
         res as Response,
@@ -43,6 +47,24 @@ describe("Given a deleteContact controller", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
       expect(res.json).toHaveBeenCalledWith(expectedJsonResponse);
+    });
+  });
+
+  describe("When it receives a request and the contact doesn't exist", () => {
+    test("Then it should call next function with 'Contact not found' error", async () => {
+      const expecteError = new CustomError(404, "Contact not found");
+
+      Contact.findOneAndDelete = jest.fn().mockReturnValue({
+        exec: jest.fn().mockResolvedValue(null),
+      });
+
+      await deleteContact(
+        req as CustomRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(expecteError);
     });
   });
 });
